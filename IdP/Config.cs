a0ea0ea46c43499bc58b590823e4431e0797fc3f
JRolandros <c.roland.akamba@gmail.com>
2023-06-05 22:28:05 -0400

    added Asp.Net app to hndleapplication users. also added web client to consume the api.

--- IdP/Config.cs
+++ IdP/Config.cs
@@ -1,13 +1,15 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace IdP;
 
 public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
-        new IdentityResource[]
+        new List<IdentityResource>
         {
-            new IdentityResources.OpenId()
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
@@ -17,8 +19,10 @@ public static class Config
         };
 
     public static IEnumerable<Client> Clients =>
+        
         new List<Client>
         {
+            //Register the backend API
             new Client
             {
                 ClientId="SugarSharkshop",
@@ -26,6 +30,25 @@ public static class Config
                 ClientSecrets={ new Secret("sugarsharc".Sha256()) },
                 AllowedScopes={ "SugarSharkApi" }, 
                 AllowAccessTokensViaBrowser=true,
+            },
+
+            //Register the web client 
+            new Client
+            {
+                ClientId="SugarSharkshop-client",
+                ClientSecrets={ new Secret("sugarsharc-client".Sha256()) },
+
+                AllowedGrantTypes=new List<string>{GrantType.AuthorizationCode },
+                
+                AllowedScopes=new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile
+                },
+
+                //Redirections
+                RedirectUris = { "https://localhost:5002/signin-oidc" }, //after login
+                PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" }, //after logout
             }
         };
 }
